8e0d4e89ec9db670aa7e95d4b714389689677c50
Kyu <rocketword@gmail.com>
2021-04-16 19:41:35 -0500

    Added enchant effects, removed addenchant command

--- src/main/java/com/emoteville/emotevillespawner/listener/SpawnerInteractListener.java
+++ src/main/java/com/emoteville/emotevillespawner/listener/SpawnerInteractListener.java
@@ -1,12 +1,18 @@
 package com.emoteville.emotevillespawner.listener;
 
 import com.emoteville.emotevillespawner.EmoteVilleSpawner;
+import com.emoteville.emotevillespawner.util.ToolHelper;
+import de.dustplanet.silkspawners.events.SilkSpawnersSpawnerBreakEvent;
+import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 public class SpawnerInteractListener implements Listener {
     private final EmoteVilleSpawner plugin;
@@ -22,11 +28,33 @@ public class SpawnerInteractListener implements Listener {
             return;
         }
 
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.SPAWNER
-        && event.getPlayer().isSneaking() && event.getHand() == EquipmentSlot.HAND) {
+        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.SPAWNER)
+        && event.getPlayer().isSneaking() && Objects.equals(event.getHand(), EquipmentSlot.HAND)) { // Main Hand
             EmoteVilleSpawner.playerInteractedSpawners.put(
                     event.getPlayer().getUniqueId(), event.getClickedBlock().getLocation());
             event.getPlayer().performCommand(this.plugin.getSpawnerCmd());
         }
+    }
+
+    @EventHandler
+    public void breakSpawnerEvent(SilkSpawnersSpawnerBreakEvent event) {
+        if (event.getPlayer() == null) {
+            return;
+        }
+
+        if (ToolHelper.isPickaxe(event.getPlayer().getInventory().getItemInMainHand())) {
+            ItemStack pickaxe = event.getPlayer().getInventory().getItemInMainHand();
+            if (pickaxe.hasItemMeta()) {
+                if (pickaxe.getItemMeta().getEnchantLevel(this.plugin.SPAWNER_ENCHANT) >= 1) {
+                    return;
+                }
+            }
+        }
+
+        event.getSpawner().getWorld().playEffect(
+                event.getSpawner().getLocation(), Effect.MOBSPAWNER_FLAMES, 2
+        );
+        event.getSpawner().getBlock().breakNaturally(null); // TODO playSound maybe?
+        event.setCancelled(true);
     }
 }
